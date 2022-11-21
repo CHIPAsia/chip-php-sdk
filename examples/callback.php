a5cc7b4cd7707a230968fe6505dce3ccdd016e8d
mamadcloud <muhammadhsn.hsn@gmail.com>
2022-11-22 01:03:41 +0800

    fix: callback verification

--- examples/callback.php
+++ examples/callback.php
@@ -7,18 +7,24 @@
   $chip = new \Chip\ChipApi($config['brand_id'], $config['api_key'], $config['endpoint']);
 
   # Option 1: Use success_callback parameter of the Purchase object
-  $post = json_decode(file_get_contents('php://input')); # lib/Model/Purchase.php
+  $post = file_get_contents('php://input'); # lib/Model/Purchase.php
   $headers = getallheaders();
   $xSignature = $headers["X-Signature"];
 
-  $verify = \Chip\ChipApi::verify(json_encode($post, JSON_PRETTY_PRINT), $xSignature, $publicKey);
+  # GET PUBLIC KEY
+  $url = $config['endpoint'] . "public_key/";
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $headers = array(
+    "Authorization: Bearer " . $config['api_key'],
+  );
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  $publicKey = json_decode(curl_exec($curl));
+  curl_close($curl);
+
+  $verify = \Chip\ChipApi::verify($post, $xSignature, $publicKey);
+  echo $verify;
 
   # Option 2: Use GET /purchases/<purchase_id>/ request
   $purchase = $chip->getPurchase($post->id);
-
-  openlog("phperrors", LOG_PID | LOG_PERROR, 0);
-  syslog(LOG_ERR, json_encode($verify));
-  syslog(LOG_PID, json_encode($post));
-  syslog(LOG_PID, $xSignature);
-  syslog(LOG_PID, $publicKey);
-  closelog();
--- examples/config.php
+++ examples/config.php
@@ -1,4 +1,7 @@
 <?php
 
 return [
+	'brand_id' => '<<BRAND_ID>>',
+	'api_key' => '<<API_KEY>>',
+	'endpoint' => 'https://gate.chip-in.asia/api/v1/'
 ];
